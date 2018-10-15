8e0a068350d7068a9ca4187036d1411ba488b937
SD07 <SD07@mitt.ca>
2018-10-15 10:13:07 -0500

    Bug Tracker app Created

---
+++ FinalBugTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalBugTracker.MvcApplication" Language="C#" %>
---
+++ FinalBugTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalBugTracker.Startup))]
+namespace FinalBugTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
